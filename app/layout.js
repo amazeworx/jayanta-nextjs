eb81b38413142d0961c49a53108e8b580101eeff
Fransnico Susanto <fransnico@amazeworx.com>
2023-05-25 17:41:34 +0700

    update gtm

--- app/layout.js
+++ app/layout.js
@@ -27,10 +27,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang='id'>
       <Script
+        id='gtm-script'
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script strategy='lazyOnload'>
+      <Script id='gtm-tag' strategy='lazyOnload'>
         {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
