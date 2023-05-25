d3635fa0b304fb38174a2c100a4f3f1a96b6621b
Fransnico Susanto <fransnico@amazeworx.com>
2023-05-25 16:27:52 +0700

    add gtm analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Barlow, Inter } from 'next/font/google';
 import Navbar from './navbar';
 import Drawer from './drawer';
 import Footer from './footer';
+import Script from 'next/script';
 
 const barlow = Barlow({
   subsets: ['latin'],
@@ -25,6 +26,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='id'>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       <body
         className={
           inter.className +
